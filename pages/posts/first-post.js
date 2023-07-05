cd0c562f6d8c8aa016fff68c6a565e7e39b9fa9f
luke7231 <tmddnjs7231@gmail.com>
2023-07-05 18:51:39 +0900

    feat:  Head 컴포넌트 적용해보기

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,12 @@
+import Head from "next/head";
+
 export default function FirstPost() {
-  return <h1>첫 번 째 글 좋아요</h1>;
+  return (
+    <>
+      <Head>
+        <title>첫 번째 헤더</title>
+      </Head>
+      <h1>첫 번 째 글 좋아요</h1>
+    </>
+  );
 }
